3256868a73e324eaac95cfb0df883be583a4248e
Vahn Gomes <Github@codycody.org>
2023-05-27 16:09:38 -0400

    🔥 chore(MainWindow.xaml.cs): remove confirmation dialog when closing the app to improve user experience

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -708,11 +708,6 @@ namespace CodeHalt
 
         private void CloseWindow(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Are you sure you want to close CodeHalt?", "Exit Setup", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.No)
-            {
-                return;
-            }
             this.Close();
         }
 
